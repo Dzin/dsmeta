8b2615371efaf23f0c678a28abe1345140577f6a
Lucas Dzin Pedroso <lucasdzin@gmail.com>
2022-07-13 14:46:22 -0300

    API test

---
+++ backend/src/main/java/com/ldp/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.ldp.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ldp.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+}
