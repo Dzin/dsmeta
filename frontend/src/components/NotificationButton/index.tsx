c96411cf108ec143fcfb6237325ab43640864210
Lucas Dzin Pedroso <lucasdzin@gmail.com>
2022-07-17 07:54:58 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-function NotificationButton() {
+function handleClick(id: number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification/`)
+            .then(response => {
+                console.log("Sucesso!");
+            });
+}
+
+type Props = {
+    saleId: number;
+}
+
+function NotificationButton({ saleId } : Props ) {
     return (
-        <div className="dsmeta-send-btn">
+        <div className="dsmeta-send-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
